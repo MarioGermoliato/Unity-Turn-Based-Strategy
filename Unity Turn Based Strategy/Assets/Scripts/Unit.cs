c9c76d10e10a6ef3f88386c5488cfcb5958f86c0
MarioGermoliato <mgermoliato@yahoo.com.br>
2023-04-17 01:17:09 -0300

    5. Unit move. Implementação do começo do script de movimento, usando vector3,distance e a posição desejada - a atual para efetuar o deslocamento. 6. Mouse raycast Implementação de um raycast que sai da camera em direção a posição do mouse 7. Mouse World Position Adição de uma função que localiza o ponto de colisão do raycast e retorna o vector3 da posição.

---
+++ Unity Turn Based Strategy/Assets/Scripts/Unit.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    [SerializeField] float moveSpeed = 4f; 
+
+    private void Update()
+    {
+        float stoppingDistance = .1f;
+
+
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance )
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+        }
+        
+
+       
+        
+
+       if(Input.GetKeyDown(KeyCode.F)) 
+        {
+            targetPosition = new Vector3(4, 0, 4);        
+        }
+        
+    }
+
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+}
