18d6bc33d1c7e308948b034719ad1f69bdcc6c19
MarioGermoliato <mgermoliato@yahoo.com.br>
2023-04-18 01:09:21 -0300

    13. Unit Rotate when Moving - Adição de rotação usando transform.foward e suavizando a rotação utilizando vector3.lerp 14. Unit selection - implementação de um sistema de seleção para lidar com multiplas units em uma cena.

--- Unity Turn Based Strategy/Assets/Scripts/Unit.cs
+++ Unity Turn Based Strategy/Assets/Scripts/Unit.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {    
     [SerializeField] float moveSpeed = 4f;
+    [SerializeField] float rotateSpeed = 10f;
     [SerializeField] Animator unitAnimator;
 
     private Vector3 targetPosition;
-    
+
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -18,25 +24,19 @@ public class Unit : MonoBehaviour
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
             unitAnimator.SetBool("IsWalking", true);
         }
         else
         {
             unitAnimator.SetBool("IsWalking", false);
-        }
-        
-
-       
-        
-
-       if(Input.GetMouseButtonDown(0)) 
-        {
-            Move(MouseWorld.GetPosition());        
-        }
+        }     
         
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
