5344270d1becf730eb525236f66479567b01adb8
MarioGermoliato <mgermoliato@yahoo.com.br>
2023-04-22 11:34:33 -0300

    17. Level Grid - Implementação do script level grid, ele é responsável por gerenciar as Units em cada GridObject. 18. Cinemachine - Adição de cinemachine ao projeto

--- Unity Turn Based Strategy/Assets/Scripts/Unit.cs
+++ Unity Turn Based Strategy/Assets/Scripts/Unit.cs
@@ -9,11 +9,18 @@ public class Unit : MonoBehaviour
     [SerializeField] Animator unitAnimator;
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -32,8 +39,15 @@ public class Unit : MonoBehaviour
         else
         {
             unitAnimator.SetBool("IsWalking", false);
-        }     
+        }
+
         
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if( newGridPosition != gridPosition)
+        { //unit changed grid position
+            LevelGrid.Instance.UnitMoveGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
     }
 
     public void Move(Vector3 targetPosition)
